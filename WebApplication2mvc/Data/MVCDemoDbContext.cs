f97669893d79232bb83c8f1fc4f022cc1019e7ec
Beknur Arystan <Beknurarystan@gmail.com>
2023-05-29 10:41:42 +0600

    Добавьте файлы проекта.

---
+++ WebApplication2mvc/Data/MVCDemoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2mvc.Models.Domain;
+
+namespace WebApplication2mvc.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees{ get; set; }
+
+    }
+}
